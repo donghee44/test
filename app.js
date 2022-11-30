f0510e7ee717a1ddbdbd62c9b3261823c12b8150
donghee44 <113874655+donghee44@users.noreply.github.com>
2022-12-01 01:45:55 +0900

    app 수정

--- app.js
+++ app.js
@@ -6,39 +6,51 @@ const server = http.createServer(app)
 const socketIo = require("socket.io");
 const moment = require("moment");
 
-const io = socketIo(server)
+const io = socketIo(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
 
 app.use(express.static(path.join(__dirname,"src")))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
+
+const chatspace = io.of("/chat")
 
 io.on("connection",(socket)=>{
     console.log(socket.id)
     const id = socket.id
     io.emit("broadcast",(socket.id))
 
-    socket.on("joinroom",(number)=>{
-        const item ={number,id}
-        socket.join(number)
-        console.log("number: ", `${number}에 입장.`)
-        io.to(number).emit("broadcast2",item)
+    socket.on("joinroom",(chatroom)=>{
+        const item ={chatroom,id}
+        socket.join(chatroom)
+        console.log("chatroom: ", `${chatroom}에 입장.`)
+        io.to(chatroom).emit("broadcast2",item)
     })
     
     socket.on("chatting",(data)=>{
-        const {name,msg,number} = data
+        const {chatroom,userId,message} = data
         console.log(data)
-        io.to(number).emit("chatting",{
-            name,
-            msg,
+        io.to(chatroom).emit("chatting",{
+            chatroom,
+            userId,
+            message,
             time:moment(new Date()).format("h:mm A")
         })
     })
+    
+    socket.on("disconnect",()=>{
+        //데이터 저장
+    })
+})
 
     // socket.on('room1',(data)=>{
     //     console.log(data)
     //     io.to('room1').emit('chatting',data)
     // })
-})
 
 
 server.listen(PORT,()=>console.log(`server is running ${PORT}`))
